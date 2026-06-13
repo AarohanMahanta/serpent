48f57e47efcacb4ea1834029e1771c44650c8653
AarohanMahanta <aarohanmahanta@gmail.com>
2026-06-13 13:38:17 +0200

    feat: add initial implementation of run in vm.py

--- interpreter/vm.py
+++ interpreter/vm.py
@@ -3,4 +3,20 @@ class VirtualMachine:
     def __init__(self):
         self.stack = []
 
-    
+    def run(self, code):
+        instructions = list(code.co_code)
+        constants = code.co_consts
+
+        i = 0
+        while i < len(instructions):
+            opcode = instructions[i]
+            i += 1
+
+            if opcode == 100:  # LOAD_CONST
+                const_index = instructions[i]
+                i += 1
+                self.stack.append(constants[const_index])
+            elif opcode == 83:  # RETURN_VALUE
+                return self.stack.pop()
+            else:
+                raise NotImplementedError(f"Opcode {opcode} not implemented")
