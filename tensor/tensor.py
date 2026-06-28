102e1c0d38b46055a31769a8666c041b416cfc24
AarohanMahanta <aarohanmahanta@gmail.com>
2026-06-28 11:07:35 +0200

    feat: add call method to Neuron class

--- tensor/tensor.py
+++ tensor/tensor.py
@@ -89,3 +89,7 @@ class Neuron:
     def __init__(self, n_inputs):
         self.weights = [Tensor(random.uniform(-1, 1)) for _ in range(n_inputs)]
         self.bias = Tensor(0.0)
+
+    def __call__(self, x):
+        weighted_sum = sum((w * x_i for w, x_i in zip(self.weights, x)), self.bias) 
+        return weighted_sum.relu()
